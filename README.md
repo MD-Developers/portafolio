30012d2b1cfe0b74178d95bf1e53629ff65901fc
Diego Perozo <perozodiego@gmail.com>
2021-05-01 09:20:10 -0400

    Init Portafolio.

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="center">
+  <a href="#" aria-hidden="true"><img src="https://raw.githubusercontent.com/MD-Developers/portafolio/main/media/thumbail.png" alt="Write-Your-Card" width="1200" /></a>
+</h1>
+
+<div align="center">
+
+
+
+</div>
+
+Nuestro portafolio donde iremos mostrando algunos de nuestros proyectos realizados.
+
+<h2><a id="credits" href="#credits" aria-hidden="true">#</a> Creditos</h2>
+
+**Creado y mantenido por [Milagros Pirona](https://github.com/MilagrosPirona) & [Diego Perozo](https://github.com/DiegoPerozo).**
+
+<h2><a id="licencia" href="#licencia" aria-hidden="true">#</a> Licencia</h2>
+
+Licensed under **[GPLv3.0](LICENSE.md)**.
---
+++ js/main.js
@@ -0,0 +1,44 @@
+var seccionesPagina = new fullpage('#fullpage', {
+	autoScrolling: true,
+	fitToSection: false,
+	fitToSectionDelay: 300,
+	easing: 'easeInOutCubic',
+	scrollingSpeed: 700,
+	css3: true,
+	easingcss3: 'ease-out',
+	loopBottom: false,
+	navigation: true,
+	menu: '#menu',
+	anchors: ['inicio', 'proyectos', 'contacto'],
+	navigationTooltips: ['Inicio', 'Proyectos', 'Contacto'],
+	showActiveTooltip: false,
+	sectionsColor : ['#000', '#c2c2c2', '#000'],
+	verticalCentered: true,
+	controlArrows: true,
+	slidesNavigation: false,
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'contacto'){
+			document.querySelector('.footer').querySelector('h2').style.opacity = 1;
+		}
+	}
+});
+
+const typed = new Typed('.typed', {
+	strings: [
+		'<i>Aplicaciones Web</i>',
+		'<i>Aplicaciones Moviles</i>',
+		'<i>Aplicaciones de Escritorio</i>',
+	],
+
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 50, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
