61cd986556f089991301a0f8fef137e5d05b235d
Diego Perozo <perozodiego@gmail.com>
2024-01-24 07:55:09 -0400

    Remove lib usage and swap to SaveFrom Helper.

--- apps/downloader/js/main.js
+++ apps/downloader/js/main.js
@@ -1,62 +1,42 @@
 let Btn = document.getElementById('btn');
 let URLinput = document.querySelector('.URL-input');
 // let serverURL = 'http://127.0.0.1:4000';
-let serverURL = 'https://milaadownloader.kuroki.repl.co';
+// let serverURL = 'https://milaadownloader.kuroki.repl.co';
 
 Btn.addEventListener('click', () => {
 	if (!URLinput.value) {
-		alert('Ingresa una URL de Youtube');
+		alert('Ingresa una URL de Youtube/Tiktok');
 	} else {
 		if(URLinput.value.startsWith('https://www.tiktok.com') || URLinput.value.startsWith('https://www.vm.tiktok.com')){
 			downloadTiktok(URLinput.value);
 		} else {
-			downloadMp4(URLinput.value);
-		}
+      if(URLinput.value.startsWith('https://www.youtube.com')){
+        downloadYoutube(URLinput.value);
+      } else 
+        alert('URL Inalida!');
+    }
+
 	}
 });
 
-async function downloadMp3(query) {
-	const res = await fetch(`${serverURL}/downloadmp3?url=${query}`);
-	if(res.status == 200) {
-    alert('Descarga en proceso...');
-    // select.value = '';
-    URLinput.value = '';
-		var a = document.createElement('a');
-  		a.href = `${serverURL}/downloadmp3?url=${query}`;
-  		a.setAttribute('download', '');
-		  a.click();
-	} else if(res.status == 400) {
-		alert("URL Invalida!");
-	}
-}
+async function downloadYoutube(query) {
+  let pos = query.indexOf('y'),
+      url = '';
 
-async function downloadMp4(query) {
-	const res = await fetch(`${serverURL}/downloadmp4?url=${query}`);
-	if(res.status == 200) {
-    alert('Descarga en proceso...');
-    // select.value = '';
-    URLinput.value = '';
-		var a = document.createElement('a');
-  		a.href = `${serverURL}/downloadmp4?url=${query}`;
-  		a.setAttribute('download', '');
-		  a.click();
-	} else if(res.status == 400) {
-		alert('URL Invalida!');
-	}
+  url = query.slice(0, pos) + 'ss' + query.slice(pos);
+  var a = document.createElement('a');
+  a.href = `${url}`;
+  a.setAttribute('target', '_blank');
+  a.setAttribute('download', '');
+  a.click();
+  URLinput.value = '';
 }
 
 async function downloadTiktok(query) {
-
-	axios.get(`${serverURL}/downloadtiktok?url=${query}`)
-  .then(function (response) {
-    alert('Descarga en proceso...');
-		URLinput.value = '';
-		var a = document.createElement('a');
-  		a.href = response.data.video.noWatermark;
-  		a.setAttribute('download', '');
-		  a.click();
-  })
-  .catch(function (error) {
-    alert("URL Invalida!");
-  });
+  var a = document.createElement('a');
+  a.href = `https://sfrom.net/${query}`;
+  a.setAttribute('target', '_blank');
+  a.setAttribute('download', '');
+  a.click();
+  URLinput.value = '';
 }
